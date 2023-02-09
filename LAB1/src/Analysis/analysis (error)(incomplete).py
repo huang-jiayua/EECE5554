4ef35e688e544ee212a3413a7cec88afad7060f4
huang-jiayua <122653763+huang-jiayua@users.noreply.github.com>
2023-02-08 20:08:52 -0500

    Update analysis (error)(incomplete).py

--- LAB1/src/Analysis/analysis (error)(incomplete).py
+++ LAB1/src/Analysis/analysis (error)(incomplete).py
@@ -4,16 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 
-bag = bagreader('/home/huangjiayua/catkin_ws/src/gps_driver/data/OcculatedSpace.bag')
+bag = bagreader('/home/huangjiayua/catkin_ws/src/gps_driver/Data/OpenSpace.bag')
 data = bag.message_by_topic('/gps')
 readings = pandas.read_csv(data)
-readings['UTM_easting'] = readings['UTM_easting'] - readings['UTM_easting'].min()
-readings['UTM_northing'] = readings['UTM_northing'] - readings['UTM_northing'].min()
 
 _, ax = bagpy.create_fig(1)
-print(len(ax))
-ax[0].scatter(x = 'UTM_easting', y = 'UTM_northing', data = readings, label = 'UTM_easting VS UTM_northing')
+
+myX = 42.3371198
+myY = -71.0905213
+
+# calculate error from my location in x and y
+readings['latitude'].substract(myX, axis = 'latitude')
+readings['longitude'].substract(myY, axis = 'longitude')
+
+# scatter plot the error in x and y
+ax[0].scatter(x = 'latitude', y = 'longitude', data = readings, s= 50, label = 'altitude vs time')
+
+# add labels
 ax[0].legend()
-ax[0].set_xlabel('UTM_easting')
-ax[0].set_ylabel('UTM_northing')
+
+# add labels for x and y axis
+ax[0].set_xlabel('X error')
+ax[0].set_ylabel('Y error')
+
+# show the plot
 plt.show()
